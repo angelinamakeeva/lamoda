2384ffd3a48fc8c3de956fd0f793213d66b50cad
angelinamakeeva <125530626+angelinamakeeva@users.noreply.github.com>
2023-10-17 07:03:44 +0300

    Update main.py

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
 Kareva Alena: 20%
-
+Makeeva Angelina 10%
 
 import requests
 from bs4 import BeautifulSoup
@@ -7,19 +7,28 @@ from bs4 import BeautifulSoup
 url = 'https://www.lamoda.ru/catalogsearch/result/?q=nike%20jordan&submit=y&gender_section=women'
 r = requests.get(url)
 text = r.text
-print(text)
+
+with open("end.txt", "w", encoding="utf-8") as file:
+    file.write(text)
+    file.write("\n")  
 
 soup = BeautifulSoup(r.text, "html.parser")
 
 products = soup.find_all("div", class_="x-product-card-description__product-name")
-for product in products:
-    print(product.text)
+with open("end.txt", "a", encoding="utf-8") as file:
+    for product in products:
+        file.write(product.text)
+        file.write("\n")
 
 brands = soup.find_all("div", class_="x-product-card-description__brand-name")
-for brand in brands:
-    print(brand.text)
+with open("end.txt", "a", encoding="utf-8") as file:
+    for brand in brands:
+        file.write(brand.text)
+        file.write("\n")
 
 prices = soup.find_all("div", class_="x-product-card-description__microdata-wrap")
-for price in prices:
-    print(price.text)
+with open("end.txt", "a", encoding="utf-8") as file:
+    for price in prices:
+        file.write(price.text)
+        file.write("\n")
 
